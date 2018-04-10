1bab8dad847349c4dfd0ea2f3777abb041e2b1b9
SyaifulAndiIrawan <32381985+syaifulandiirawan@users.noreply.github.com>
2018-04-10 14:14:24 +0800

    buat method cetakinfo()

--- src/praktek3/rokok_filter.java
+++ src/praktek3/rokok_filter.java
@@ -4,4 +4,10 @@ public class rokok_filter {
     String merk;
     String warna;
     double harga;
+    
+    void cetakInfo(){
+        System.out.println("merk \t: "+merk+"\n"+
+                "warna \t: "+warna+"\n"+
+                "harga \t: "+harga);
+    }
 }
