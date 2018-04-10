ea8a8d52af4f067b466602e39c91e74bb8be71ed
SyaifulAndiIrawan <32381985+syaifulandiirawan@users.noreply.github.com>
2018-04-10 14:36:45 +0800

    Instance, buat atribut dan panggil method

--- src/praktek3/rokok_filterAksi.java
+++ src/praktek3/rokok_filterAksi.java
@@ -1,6 +1,11 @@
 package praktek3;
 public class rokok_filterAksi {
     public static void main(String[] args) {
+        rokok_filter f = new rokok_filter();
+        f.merk="sampoerna";
+        f.warna="putih";
+        f.harga=20000;
         
+        f.cetakInfo();
     } 
 }
